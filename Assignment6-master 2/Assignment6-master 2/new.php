3adb6f15b58b5ae9c46f31da5be7641a41db5b31
MichaelHershman <55210436+MichaelHershman@users.noreply.github.com>
2019-11-20 14:54:48 -0500

    Delete new.php

--- Assignment6-master 2/Assignment6-master 2/new.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('connect-db.php');
-$name = mysqli_real_escape_string($connection, htmlspecialchars($_POST['name']));
-$email = mysqli_real_escape_string($connection, htmlspecialchars($_POST['email']));
-$message = mysqli_real_escape_string($connection, htmlspecialchars($_POST['message']));
-mysqli_query($connection, "INSERT INTO rome (name, email, message) VALUES ('$name', '$email', '$message')");
-
-mysqli_close($connection);
-?>
