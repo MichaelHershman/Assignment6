eb1f658a170480a778555244e860f03a849f24c4
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-11-20 16:43:05 -0500

    Add files via upload

---
+++ new.php
@@ -0,0 +1,9 @@
+<?php
+include('connect-db.php');
+$name = mysqli_real_escape_string($connection, htmlspecialchars($_POST['name']));
+$email = mysqli_real_escape_string($connection, htmlspecialchars($_POST['email']));
+$message = mysqli_real_escape_string($connection, htmlspecialchars($_POST['message']));
+mysqli_query($connection, "INSERT INTO brooklyn (name, email, message) VALUES ('$name', '$email', '$message')");
+
+mysqli_close($connection);
+?>
