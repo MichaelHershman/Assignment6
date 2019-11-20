af5365c7e69f03eaa45aed609370a959bf470be3
MichaelHershman <55210436+MichaelHershman@users.noreply.github.com>
2019-11-20 14:25:25 -0500

    Add files via upload

---
+++ Assignment6-master 2/eboard.php
@@ -0,0 +1,64 @@
+<?php include "inc/html-top.php"; ?>
+
+
+<body>
+    <!-- SECOND PAGE -->
+
+
+    <div class="container">
+       <h2 class="top"> E-board: </h2>
+    </div>
+
+        <main class="container">
+
+        <h3> President </h3>
+
+            <p>Name</p>
+            <img src="images/ai.jpg" class="rounded" alt="">
+            <p>Is responsible for general management of club.</p>
+            <p>Combat Robotics is a team under UR Robotics that builds 1-30lb combat robots for various competitions. No prior experience required, but enthusiasm wanted. This is primarily an electro-mechanical project with a heavy emphasis on mechanical fabrication, but there are a lot of areas to explore within like 3D modeling and simulation, project engineering, solid mechanics, electrical design and assembly, and 3D printing. We are looking to take on people's ideas for building some smaller robots this year. If you have any questions, please email our team leader Daniel. </p>
+
+
+    <h3> Workshop Coordinator </h3>
+
+            <p>Name</p>
+            <img src="images/ai.jpg" class="rounded" alt="">
+            <p>Organizes workshops</p>
+            <p>Combat Robotics is a team under UR Robotics that builds 1-30lb combat robots for various competitions. No prior experience required, but enthusiasm wanted. This is primarily an electro-mechanical project with a heavy emphasis on mechanical fabrication, but there are a lot of areas to explore within like 3D modeling and simulation, project engineering, solid mechanics, electrical design and assembly, and 3D printing. We are looking to take on people's ideas for building some smaller robots this year. If you have any questions, please email our team leader Daniel. </p>
+
+    <h3>Lab Coordinator</h3>
+
+            <p>Name</p>
+            <img src="images/ai.jpg" class="rounded" alt="">
+            <p>Maintains the lab</p>
+            <p>Combat Robotics is a team under UR Robotics that builds 1-30lb combat robots for various competitions. No prior experience required, but enthusiasm wanted. This is primarily an electro-mechanical project with a heavy emphasis on mechanical fabrication, but there are a lot of areas to explore within like 3D modeling and simulation, project engineering, solid mechanics, electrical design and assembly, and 3D printing. We are looking to take on people's ideas for building some smaller robots this year. If you have any questions, please email our team leader Daniel. </p>
+
+    <h3>Vice-President</h3>
+
+            <p>Name</p>
+            <img src="images/ai.jpg" class="rounded" alt="">
+            <p>Aides in the running of the club</p>
+            <p>Combat Robotics is a team under UR Robotics that builds 1-30lb combat robots for various competitions. No prior experience required, but enthusiasm wanted. This is primarily an electro-mechanical project with a heavy emphasis on mechanical fabrication, but there are a lot of areas to explore within like 3D modeling and simulation, project engineering, solid mechanics, electrical design and assembly, and 3D printing. We are looking to take on people's ideas for building some smaller robots this year. If you have any questions, please email our team leader Daniel. </p>
+
+   <h3>Secretary</h3>
+
+            <p>Name</p>
+            <img src="images/ai.jpg" class="rounded" alt="">
+            <p>Responsible for external correspondence</p>
+            <p>Combat Robotics is a team under UR Robotics that builds 1-30lb combat robots for various competitions. No prior experience required, but enthusiasm wanted. This is primarily an electro-mechanical project with a heavy emphasis on mechanical fabrication, but there are a lot of areas to explore within like 3D modeling and simulation, project engineering, solid mechanics, electrical design and assembly, and 3D printing. We are looking to take on people's ideas for building some smaller robots this year. If you have any questions, please email our team leader Daniel. </p>
+
+
+     <h3>Business Manager</h3>
+
+            <p>Name</p>
+            <img src="images/ai.jpg" class="rounded" alt="">
+            <p>Budgetary functions of the club</p>
+            <p>Combat Robotics is a team under UR Robotics that builds 1-30lb combat robots for various competitions. No prior experience required, but enthusiasm wanted. This is primarily an electro-mechanical project with a heavy emphasis on mechanical fabrication, but there are a lot of areas to explore within like 3D modeling and simulation, project engineering, solid mechanics, electrical design and assembly, and 3D printing. We are looking to take on people's ideas for building some smaller robots this year. If you have any questions, please email our team leader Daniel. </p>
+
+    </main>
+
+
+<footer class="bg-primary">
+    <p class="text-center text-white"> UR Robotics 2019 </p>
+</footer>
+</body>
